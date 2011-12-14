db784b615f26c65a5a83a2b1ab9b84500978413e
Adnan Akil <adnanakil@gmail.com>
2011-12-14 13:18:43 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adnan::Application.config.secret_token = 'af1828b5ddb4104bf31b599312cccdccef00f6e3ae65190ea78517ccf3b2cbe8dc8b9bccf4ce57309ca72fdbcd4e46e48cb61ec6664693cee0286443819dda92'
